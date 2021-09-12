b39ca1f630b2f21364043a9be635c6cd41d1e219
Vrun <vrun4750@gmail.com>
2021-09-12 14:30:21 +0530

    init

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,10 @@
-import React, {useState, useEffect} from 'react'
+import React from 'react'
 import styled from "styled-components"
 //import { useForm } from 'react-hook-form';
 import HeaderSection from './mid';
 import Navbar from './navbar';
 import Footer from './footer';
-//import Shorty from './short';
+import Shorty from './short';
 import Advance from './midTwo';
 //import Nav from "./navbar";
 
